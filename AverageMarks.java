7a095f74604f784e187eff69ea64131fd5513342
Bharatlal <127212425+bharatlal124@users.noreply.github.com>
2023-12-11 21:31:42 +0530

    Add files via upload

---
+++ AverageMarks.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AverageMarks {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char name = sc.next().charAt(0);
+
+        int m1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int m3 = sc.nextInt();
+
+        int average = (m1 + m2 + m3) / 3;
+        System.out.println(name);
+        System.out.println(average);
+    }
+}
