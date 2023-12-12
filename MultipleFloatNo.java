f67d2eb9b41f3ea7ae3ce30a219d2e94ba6001ed
Bharatlal <127212425+bharatlal124@users.noreply.github.com>
2023-12-12 23:55:08 +0530

    Add files via upload

---
+++ MultipleFloatNo.java
@@ -0,0 +1,12 @@
+public class MultipleFloatNo {
+
+    public static double multipleNo(double f1, double f2) {
+        double product = f1 * f2;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        double result = multipleNo(1.5, 44.5);
+        System.out.println(result);
+    }
+}
---
+++ PrintASCII.java
@@ -0,0 +1,13 @@
+public class PrintASCII {
+
+    // public static int printASCI(char ch) {
+    // int i = ch;
+    // return i;
+    // }
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        // char result = printASCI(a);
+        System.out.println("ASCII value of " + ch + " is : " + (int) (ch));
+    }
+}
